f397bf4f81526c38d6b37c10bef7f32f718344ba
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-03-02 23:27:55 +0530

    Complete home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,30 @@
+import { useNavigate } from "react-router";
+import hero from "../../assets/hero.png";
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <main className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.heading}>
+          ONLINE <span className={styles.highlight}>SHOPPING</span>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi sit minus, earum cumque
+          veniam incidunt architecto. Veritatis earum laudantium blanditiis fuga at. Dolores libero
+          aliquam necessitatibus, modi qui error quaerat.
+        </p>
+        <button
+          className={styles.btn}
+          onClick={() => navigate("/products", { viewTransition: true })}
+        >
+          Shop Now
+        </button>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src={hero} alt="Hero image" />
+      </div>
+    </main>
+  );
 }
